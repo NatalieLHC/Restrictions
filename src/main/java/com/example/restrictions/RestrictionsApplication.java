f456a20ca7fc5647067042d5808ebbc72cfd0a7b
Natalie LHC <bobspange5@gmail.com>
2023-07-05 11:29:36 +0400

    initial commit

---
+++ src/main/java/com/example/restrictions/RestrictionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.restrictions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestrictionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestrictionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restrictions/RestrictionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restrictions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrictionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
